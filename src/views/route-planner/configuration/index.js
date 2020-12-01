14fb1fe7981c146ec174582b245925ed6b407df3
fazlerabi <fazle.rabbi.dev@gmail.com>
2020-12-01 07:42:12 -0500

    Add card to configuration

--- src/views/route-planner/configuration/index.js
+++ src/views/route-planner/configuration/index.js
@@ -160,199 +160,217 @@ function Configuration() {
 
   return (
     <>
-      <CTabs
-        activeTab={active}
-        color="dark"
-        className="border"
-        onActiveTabChange={(idx) => setActive(idx)}
-      >
-        <CNav variant="tabs">
-          <CDropdown inNav>
-            <CNavLink className="p-0">
-              <CDropdownToggle className="border-0" caret>
-                <FaPlus />
-                &nbsp;Import & Reload
-              </CDropdownToggle>
-            </CNavLink>
-            <CDropdownMenu>
-              <CDropdownItem>Import Excel File</CDropdownItem>
-              <CDropdownItem>Bulk Edit</CDropdownItem>
-              <CDropdownItem>Reload Saved Routes</CDropdownItem>
-              <CDropdownItem>Try us with demo addresses</CDropdownItem>
-              <CDropdownDivider />
-              <CDropdownItem>Add new stop</CDropdownItem>
-            </CDropdownMenu>
-          </CDropdown>
-          <CNavItem>
-            <CNavLink>
-              <FaAddressBook />
-              &nbsp;Address
-            </CNavLink>
-          </CNavItem>
-          <CNavItem>
-            <CNavLink>
-              <FaRoute />
-              &nbsp;Routes
-            </CNavLink>
-          </CNavItem>
-        </CNav>
-        <CTabContent className="configuration-content pt-4">
-          <CTabPane>
-            <CCard className="mx-4 shadow">
-              <CCardBody>
-                <div className="address-forms container-fluid">
-                  <AddressForm withLabel={true} index="H" />
-                  <CRow>
-                    <CCol xs="12" md="12">
-                      <div className="d-flex set-end-address mb-2">
-                        <CSwitch
-                          className="mx-1"
-                          color="primary"
-                          labelOn={"\u2713"}
-                          labelOff={"\u2715"}
-                          size="sm"
-                          checked={endAddress}
-                          onClick={toggleEndAddress}
-                        />
-                        <span className="ml-2">
-                          Set End Address or Return Route
-                        </span>
-                      </div>
-                    </CCol>
-                  </CRow>
-                  {endAddress && <AddressForm withLabel={false} index="E" />}
-                </div>
-              </CCardBody>
-            </CCard>
-            <CCard className="mx-4 address-table-card shadow">
-              <CCardBody className="data-table">
-                <CDataTable
-                  items={usersData}
-                  fields={fields}
-                  columnFilter
-                  tableFilter
-                  itemsPerPage={5}
-                  hover
-                  sorter
-                  pagination
-                  scopedSlots={{
-                    no: (item, index) => {
-                      return (
-                        <td classname="py-2">
-                          <CBadge color="primary">{item.no}</CBadge>
-                        </td>
-                      );
-                    },
-                    show_details: (item, index) => {
-                      return (
-                        <td className="py-2">
-                          <CButton
-                            color="primary"
-                            variant="outline"
-                            shape="square"
-                            size="sm"
-                            onClick={() => {
-                              toggleDetails(index);
-                            }}
-                          >
-                            {details.includes(index) ? "Hide" : "Show"}
-                          </CButton>
-                        </td>
-                      );
-                    },
-                    details: (item, index) => {
-                      return (
-                        <CCollapse show={details.includes(index)}>
-                          <CCardBody>
-                            <CButton
-                              size="sm"
+      <CCard className="h-100 shadow-sm">
+        <CCardBody>
+          <CTabs
+            activeTab={active}
+            color="dark"
+            className="border"
+            onActiveTabChange={(idx) => setActive(idx)}
+          >
+            <CNav variant="tabs">
+              <CDropdown inNav>
+                <CNavLink className="p-0">
+                  <CDropdownToggle className="border-0" caret>
+                    <FaPlus />
+                    &nbsp;Import & Reload
+                  </CDropdownToggle>
+                </CNavLink>
+                <CDropdownMenu>
+                  <CDropdownItem>Import Excel File</CDropdownItem>
+                  <CDropdownItem>Bulk Edit</CDropdownItem>
+                  <CDropdownItem>Reload Saved Routes</CDropdownItem>
+                  <CDropdownItem>Try us with demo addresses</CDropdownItem>
+                  <CDropdownDivider />
+                  <CDropdownItem>Add new stop</CDropdownItem>
+                </CDropdownMenu>
+              </CDropdown>
+              <CNavItem>
+                <CNavLink>
+                  <FaAddressBook />
+                  &nbsp;Address
+                </CNavLink>
+              </CNavItem>
+              <CNavItem>
+                <CNavLink>
+                  <FaRoute />
+                  &nbsp;Routes
+                </CNavLink>
+              </CNavItem>
+            </CNav>
+            <CTabContent className="configuration-content pt-4">
+              <CTabPane>
+                <CCard className="mx-4 shadow">
+                  <CCardBody>
+                    <div className="address-forms container-fluid">
+                      <AddressForm withLabel={true} index="H" />
+                      <CRow>
+                        <CCol xs="12" md="12">
+                          <div className="d-flex set-end-address mb-2">
+                            <CSwitch
+                              className="mx-1"
                               color="primary"
-                              onClick={() => {
-                                setStopOpen(true);
-                              }}
-                            >
-                              Edit Stop
-                            </CButton>
-                            <CButton size="sm" color="danger" className="ml-1">
-                              Delete
-                            </CButton>
-                          </CCardBody>
-                        </CCollapse>
-                      );
-                    },
-                  }}
-                />
-              </CCardBody>
-            </CCard>
-          </CTabPane>
-          <CTabPane className="h-100">
-            <Goal />
-          </CTabPane>
-          <CTabPane className="h-100">
-            <div className="d-flex flex-column px-3 h-100">
-              <div className="my-auto">
-                <p className="mb-3">
-                  Welcome to our <b>Multiple Stop Route Planner</b>. A route
-                  mapping software to find the best route and navigate with
-                  driving directions
-                </p>
-                <p className="mb-0">
-                  1. Insert addresses, using house number, street, city, state
-                  and zip code.
-                </p>
-                <p className="mb-0">
-                  2. Click 'Set Goals' to include features like service time or
-                  multi-routing.
-                </p>
-                <p className="mb-0">
-                  3. Click 'Plan My Route' to create the best multi-stop route.
-                </p>
-                <p>4. Navigate with MyRoute app</p>
-              </div>
-            </div>
-          </CTabPane>
-        </CTabContent>
-      </CTabs>
-      <CModal show={stopOpen} onClose={() => setStopOpen(!stopOpen)} size="lg">
-        <CModalHeader closeButton>
-          <CModalTitle>Edit Stop</CModalTitle>
-        </CModalHeader>
-        <CModalBody>
-          <CFormGroup>
-            <CLabel htmlFor="title">Title</CLabel>
-            <CInput id="title" placeholder="Enter your stop title" />
-          </CFormGroup>
-          <CFormGroup>
-            <CLabel htmlFor="address">Address</CLabel>
-            <CInput id="address" />
-          </CFormGroup>
-          <CFormGroup row className="my-0">
-            <CCol xs="6">
+                              labelOn={"\u2713"}
+                              labelOff={"\u2715"}
+                              size="sm"
+                              checked={endAddress}
+                              onClick={toggleEndAddress}
+                            />
+                            <span className="ml-2">
+                              Set End Address or Return Route
+                            </span>
+                          </div>
+                        </CCol>
+                      </CRow>
+                      {endAddress && (
+                        <AddressForm withLabel={false} index="E" />
+                      )}
+                    </div>
+                  </CCardBody>
+                </CCard>
+                <CCard className="mx-4 address-table-card shadow">
+                  <CCardBody className="data-table">
+                    <CDataTable
+                      items={usersData}
+                      fields={fields}
+                      columnFilter
+                      tableFilter
+                      itemsPerPage={5}
+                      hover
+                      sorter
+                      pagination
+                      scopedSlots={{
+                        no: (item, index) => {
+                          return (
+                            <td classname="py-2">
+                              <CBadge color="primary">{item.no}</CBadge>
+                            </td>
+                          );
+                        },
+                        show_details: (item, index) => {
+                          return (
+                            <td className="py-2">
+                              <CButton
+                                color="primary"
+                                variant="outline"
+                                shape="square"
+                                size="sm"
+                                onClick={() => {
+                                  toggleDetails(index);
+                                }}
+                              >
+                                {details.includes(index) ? "Hide" : "Show"}
+                              </CButton>
+                            </td>
+                          );
+                        },
+                        details: (item, index) => {
+                          return (
+                            <CCollapse show={details.includes(index)}>
+                              <CCardBody>
+                                <CButton
+                                  size="sm"
+                                  color="primary"
+                                  onClick={() => {
+                                    setStopOpen(true);
+                                  }}
+                                >
+                                  Edit Stop
+                                </CButton>
+                                <CButton
+                                  size="sm"
+                                  color="danger"
+                                  className="ml-1"
+                                >
+                                  Delete
+                                </CButton>
+                              </CCardBody>
+                            </CCollapse>
+                          );
+                        },
+                      }}
+                    />
+                  </CCardBody>
+                </CCard>
+              </CTabPane>
+              <CTabPane className="h-100">
+                <Goal />
+              </CTabPane>
+              <CTabPane className="h-100">
+                <div className="d-flex flex-column px-3 h-100">
+                  <div className="my-auto">
+                    <p className="mb-3">
+                      Welcome to our <b>Multiple Stop Route Planner</b>. A route
+                      mapping software to find the best route and navigate with
+                      driving directions
+                    </p>
+                    <p className="mb-0">
+                      1. Insert addresses, using house number, street, city,
+                      state and zip code.
+                    </p>
+                    <p className="mb-0">
+                      2. Click 'Set Goals' to include features like service time
+                      or multi-routing.
+                    </p>
+                    <p className="mb-0">
+                      3. Click 'Plan My Route' to create the best multi-stop
+                      route.
+                    </p>
+                    <p>4. Navigate with MyRoute app</p>
+                  </div>
+                </div>
+              </CTabPane>
+            </CTabContent>
+          </CTabs>
+          <CModal
+            show={stopOpen}
+            onClose={() => setStopOpen(!stopOpen)}
+            size="lg"
+          >
+            <CModalHeader closeButton>
+              <CModalTitle>Edit Stop</CModalTitle>
+            </CModalHeader>
+            <CModalBody>
               <CFormGroup>
-                <CLabel htmlFor="order-size">Order Size</CLabel>
-                <CInput id="order-size" placeholder="Enter your order size" />
+                <CLabel htmlFor="title">Title</CLabel>
+                <CInput id="title" placeholder="Enter your stop title" />
               </CFormGroup>
-            </CCol>
-            <CCol xs="6">
               <CFormGroup>
-                <CLabel htmlFor="service-time">Service Time</CLabel>
-                <CInput
-                  id="service-time"
-                  placeholder="Enter your service time"
-                />
+                <CLabel htmlFor="address">Address</CLabel>
+                <CInput id="address" />
               </CFormGroup>
-            </CCol>
-          </CFormGroup>
-        </CModalBody>
-        <CModalFooter>
-          <CButton color="primary" onClick={() => setStopOpen(!stopOpen)}>
-            Save
-          </CButton>{" "}
-          <CButton color="secondary" onClick={() => setStopOpen(!stopOpen)}>
-            Cancel
-          </CButton>
-        </CModalFooter>
-      </CModal>
+              <CFormGroup row className="my-0">
+                <CCol xs="6">
+                  <CFormGroup>
+                    <CLabel htmlFor="order-size">Order Size</CLabel>
+                    <CInput
+                      id="order-size"
+                      placeholder="Enter your order size"
+                    />
+                  </CFormGroup>
+                </CCol>
+                <CCol xs="6">
+                  <CFormGroup>
+                    <CLabel htmlFor="service-time">Service Time</CLabel>
+                    <CInput
+                      id="service-time"
+                      placeholder="Enter your service time"
+                    />
+                  </CFormGroup>
+                </CCol>
+              </CFormGroup>
+            </CModalBody>
+            <CModalFooter>
+              <CButton color="primary" onClick={() => setStopOpen(!stopOpen)}>
+                Save
+              </CButton>{" "}
+              <CButton color="secondary" onClick={() => setStopOpen(!stopOpen)}>
+                Cancel
+              </CButton>
+            </CModalFooter>
+          </CModal>
+        </CCardBody>
+      </CCard>
     </>
   );
 }
